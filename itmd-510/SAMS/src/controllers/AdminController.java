a99ad51426c76bfdc47462786a646b5cea23f3c7
dvenkatesalu <dharanip.priya@gmail.com>
2018-12-06 14:18:40 -0600

    Admin View : Initial Commit

--- itmd-510/SAMS/src/controllers/AdminController.java
+++ itmd-510/SAMS/src/controllers/AdminController.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import models.AdminModel;
+
 /**
  * AdminController.java
  * @author Dharanip Priya
@@ -8,13 +16,61 @@ package controllers;
  */
 public class AdminController 
 {
-
+	private ObservableList<String> actionList = FXCollections.observableArrayList("View","Modify","Delete","Enter");
+	private ObservableList<String> targetList = FXCollections.observableArrayList("Student", "Instructor", "Course");
+	private ObservableList<String> functionList = FXCollections.observableArrayList("Details","Schedule");
+	private ObservableList<String> deptList = FXCollections.observableArrayList("ITM", "MMAE", "ECE", "CSE");
+	
+	@FXML
+	private ComboBox<String> actionDrpDwn;
+	@FXML
+	private ComboBox<String> targetDrpDwn;
+	@FXML
+	private ComboBox<String> functionDrpDwn;
+	@FXML
+	private Button ftchDtls;
+	
+	@FXML
+	private AnchorPane adminGoView;
+	@FXML
+	private ComboBox<String> deptDrpDwn;
+	@FXML 
+	private ComboBox<String> cwidDrpDwn;
+	
+	private AdminModel admin;
+	
 	/**
 	 * 
 	 */
 	public AdminController() 
 	{
-		// TODO Auto-generated constructor stub
+		admin = new admin();
+	}
+	
+	public void initialize()
+	{
+		actionDrpDwn.setItems(actionList);
+		targetDrpDwn.setItems(targetList);
+		functionDrpDwn.setItems(functionList);
+		deptDrpDwn.setItems(deptList);
+		adminGoView.setVisible(false);
+	}
+	
+	public void fetchDetails()
+	{
+		//Check for null or empty before proceeding
+		adminGoView.setVisible(true);
+		
+		if( targetDrpDwn.getAccessibleText().equals("Course"))
+		{
+			
+		}
+		
+		else
+		{
+			ObservableList<String> cwidList = FXCollections.observableArrayList(admin.getCWIDs(deptDrpDwn.getAccessibleText(), targetDrpDwn.getAccessibleText()));
+			cwidDrpDwn.setItems(cwidList);
+		}
 	}
 
 }
---
+++ itmd-510/SAMS/src/models/AdminModel.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.util.ArrayList;
+
+/**
+ * AdminModel.java
+ * @author Dharanip Priya
+ * @author Harika Kuladevi
+ * @since Dec 02, 2018
+ */
+
+public class AdminModel extends Person
+{
+
+	/**
+	 * 
+	 */
+	public AdminModel() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public void enroll() 
+	{
+		
+	}
+	
+	public void generateInstructorrepo() 
+	{
+		
+	}
+	public void generateStudentrepo() 
+	{
+		
+	}
+	
+	/*public ArrayList<String> getCWIDs( String dept, String personType)
+	{
+		//Need to check if its a student or instructor and need to query accordingly
+	}
+	
+	public ArrayList<String> getCourseIDs( String dept )
+	{
+		
+	}*/
+}
