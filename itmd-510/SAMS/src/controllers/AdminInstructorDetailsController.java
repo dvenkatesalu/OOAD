4c8f0b6dde194f257f3c03d3314d419b505995ac
visagan97 <vguruparan@hawk.iit.edu>
2018-12-07 18:14:40 -0600

    AdminInstructorDetailsController.java AdminInstructorDetailsView.fxml

---
+++ itmd-510/SAMS/src/controllers/AdminInstructorDetailsController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package controllers;
+
+/**
+ * @author Admin
+ *
+ */
+public class AdminInstructorDetailsController {
+
+	/**
+	 * 
+	 */
+	public AdminInstructorDetailsController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
